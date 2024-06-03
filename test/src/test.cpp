67a363a95157092f726aa6c2a06493270bffdb7b
Jishan Ahamed <jjahamed33@gmail.com>
2024-06-03 23:38:46 +0530

    vec3 test added

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,6 +1,19 @@
 #include "vec2_test.h"
+#include "vec3_test.h"
+
+void test_vec2();
+
+void test_vec3();
 
 int main() {
+    
+    test_vec3();
+
+    return 0;
+ }
+
+void test_vec2()
+{
     Vec2Test test;
 
     test.additionTest(vec2(1.0f, 2.0f), vec2(3.0f, 4.0f), vec2(4.0f, 6.0f));
@@ -21,6 +34,28 @@ int main() {
 
     // Run all tests
     test.run();
+}
 
-    return 0;
- }
+void test_vec3()
+{
+    Vec3Test test;
+
+    test.additionTest(vec3(1.0f, 2.0f, 3.0f), vec3(3.0f, 4.0f, 5.0f), vec3(4.0f, 6.0f, 8.0f));
+
+    test.subtractionTest(vec3(5.0f, 8.0f, 10.0f), vec3(2.0f, 3.0f, 4.0f), vec3(3.0f, 5.0f, 6.0f));
+
+    test.scalarMultiplicationTest(vec3(2.0f, 3.0f, 4.0f), 2, vec3(4.0f, 6.0f, 8.0f));
+
+    test.scalarDivisionTest(vec3(4.0f, 6.0f, 8.0f), 2, vec3(2.0f, 3.0f, 4.0f));
+
+    test.normalizationTest(vec3(3.0f, 4.0f, 5.0f), vec3(0.424264f, 0.565685f, 0.707107f));
+
+    test.magnitudeTest(vec3(3.0f, 4.0f, 5.0f), 7.071068f);
+
+    test.dotProductTest(vec3(3, 4, 5), vec3(2, -1, 3), 17);
+
+    test.crossProductTest(vec3(2, 3, 4), vec3(-1, 2, 1), vec3(-5, -6, 7));
+
+    // Run all tests
+    test.run();
+}
---
+++ test/src/vec3_test.h
@@ -0,0 +1,131 @@
+#pragma once
+
+#include "test.h"
+#include "g3d/vec3.h"
+
+
+using namespace g3d;
+
+class Vec3Test : public test
+{
+public:
+	void additionTest(const vec3& v1, const vec3& v2, const vec3& expectedResult)
+	{
+		addTestCase("Addition Test", [v1, v2, expectedResult]()
+			{
+				vec3 result = v1 + v2;
+
+				std::cout << v1 << " + " << v2 << " = ";
+				std::cout << result << "\n";
+
+				if (result.x != expectedResult.x || result.y != expectedResult.y) {
+					throw std::runtime_error("Addition test failed!");
+				}
+			});
+	}
+
+	void subtractionTest(const vec3& v1, const vec3& v2, const vec3& expectedResult)
+	{
+		addTestCase("Subtraction Test", [v1, v2, expectedResult]()
+			{
+				vec3 result = v1 - v2;
+
+				std::cout << v1 << " - " << v2 << " = ";
+				std::cout << result << "\n";
+
+				if (result.x != expectedResult.x || result.y != expectedResult.y) {
+					throw std::runtime_error("Subtraction test failed!");
+				}
+			});
+	}
+
+	void scalarMultiplicationTest(const vec3& v1, const int scalar, const vec3& expectedResult)
+	{
+		addTestCase("Scalar Multiplication Test", [v1, scalar, expectedResult]()
+			{
+				vec3 result = v1 * scalar;
+
+				std::cout << v1 << " * " << scalar << " = ";
+				std::cout << result << "\n";
+
+				if (result.x != expectedResult.x || result.y != expectedResult.y) {
+					throw std::runtime_error("Scalar Multiplication test failed!");
+				}
+			});
+	}
+
+	void scalarDivisionTest(const vec3& v1, const int scalar, const vec3& expectedResult)
+	{
+		addTestCase("Scalar Division Test", [v1, scalar, expectedResult, this]()
+			{
+				vec3 result = v1 / scalar;
+
+				std::cout << v1 << " / " << scalar << " = ";
+				std::cout << result << "\n";
+
+				if (!isAlmostEqual(result.x, expectedResult.x) && !isAlmostEqual(result.y, expectedResult.y)) {
+					throw std::runtime_error("Scalar Division test failed!");
+				}
+			});
+	}
+
+	void normalizationTest(const vec3& v, const vec3& expectedResult)
+	{
+		addTestCase("Normalization Test", [v, expectedResult, this]()
+			{
+				vec3 normal = v.normalized();
+
+				std::cout << "Original vector: " << v << "\n";
+				std::cout << "Normalized vector: " << normal << "\n";
+
+				if (!isAlmostEqual(normal.x, expectedResult.x) && !isAlmostEqual(normal.y, expectedResult.y)) {
+					throw std::runtime_error("Normalization test failed!");
+				}
+			});
+	}
+
+	void magnitudeTest(const vec3& v, float expectedResult)
+	{
+		addTestCase("Magnitude Test", [v, expectedResult, this]()
+			{
+				float m = v.magnitude();
+
+				std::cout << "Magnitude of " << v;
+				std::cout << " = " << m << "\n";
+
+				if (!isAlmostEqual(m, expectedResult)) {
+					throw std::runtime_error("Normalization test failed!");
+				}
+			});
+	}
+
+	void dotProductTest(const vec3& v1, const vec3& v2, const float expectedResult)
+	{
+		addTestCase("Dot Product Test", [v1, v2, expectedResult]()
+			{
+				float result = vec3::dot(v1, v2);
+
+				std::cout << v1 << " . " << v2 << " = ";
+				std::cout << result << "\n";
+
+				if (result != expectedResult) {
+					throw std::runtime_error("Dot Product test failed!");
+				}
+			});
+	}
+
+	void crossProductTest(const vec3& v1, const vec3& v2, const vec3& expectedResult)
+	{
+		addTestCase("Cross Product Test", [v1, v2, expectedResult]()
+			{
+				vec3 result = vec3::cross(v1, v2);
+
+				std::cout << v1 << " x " << v2 << " = ";
+				std::cout << result << "\n";
+
+				if (result.x != expectedResult.x || result.y != expectedResult.y || result.z != expectedResult.z) {
+					throw std::runtime_error("Cross Product test failed!");
+				}
+			});
+	}
+};
