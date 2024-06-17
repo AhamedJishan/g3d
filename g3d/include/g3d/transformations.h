906c09cea0633731127e5f5ccdfeb5c8b42bfe5c
Jishan Ahamed <jjahamed33@gmail.com>
2024-06-18 00:27:48 +0530

    tranformations file created

---
+++ g3d/include/g3d/transformations.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "vec3.h"
+#include "mat3.h"
+
+namespace g3d
+{
+	mat3 scale(const mat3& mat, const vec3& vec);
+}
---
+++ g3d/src/transformations.cpp
@@ -0,0 +1,10 @@
+#include "g3d/transformations.h"
+
+namespace g3d
+{
+	mat3 scale(const mat3& mat, const vec3& vec)
+	{
+
+		return mat3();
+	}
+}
